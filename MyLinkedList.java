8979e397832c1853c3d26ab53f2aee8ff3eb10c5
Jonah Keller <jkeller10@stuy.edu>
2021-01-08 01:29:57 -0500

    I'm sort of lost

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,31 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
- public boolean add(String value);
- public void add(int index, String value); 
+ public MyLinkedList(){
+  size = 0;
+  start = null;
+  end = null;
+}
 
- public String get(int index);
+ public int size()
+ {
+   return size;
+ }
+ public boolean add(String value)
+ {
+   for(int i = 0; i < size; i++)
+   {
+
+   }
+ }
+ public void add(int index, String value);
+ public String get(int index)
+ {
+   if(index >= this.size() || index < 0)
+   {
+     throw new IndexOutOfBoundsException(); 
+   }
+ }
  public String set(int index, String value);
  public String toString();
  //Any helper method that returns a Node object MUST BE PRIVATE!
--- Node.java
+++ Node.java
@@ -1,6 +1,43 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
+ public Node(String value)
+{
+  /*create a constructor*/
+  data = value;
+  next = null;
+  prev = null;
+}
  private String data;
  private Node next,prev;
+
+
  //write get/set methods for all three instance variables.
+
+ public String get(int index){
+   return data;
+ }
+ public String getNext(int index)
+ {
+   return next.get(index);
+ }
+ public String getPrev(int index)
+ {
+   return prev.get(index);
+ }
+
+ public String set(String value)
+ {
+   data = value;
+   return value;
+ }
+
+ public String setNext(String value)
+ {
+   return next.set(value);
+ }
+
+ public String setPrev(String value)
+ {
+   return prev.set(value);
+ }
+
 }
