43bc1d514d134d7e35268654dcac1a48aa41397a
Jonah Keller <jkeller10@stuy.edu>
2021-01-10 22:48:11 -0500

    remove left

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -15,20 +15,66 @@ public class MyLinkedList{
  public boolean add(String value)
  {
    size ++;
-   NodeIndex(size-2).setNext(value);
-   NodeIndex(size-1).setNext(null);
-   return true;
 
+   if(size == 1)
+   {
+     NodeIndex(0).setNext(null);
+     NodeIndex(0).setPrev(null);
+     NodeIndex(0).set(value);
+     start = NodeIndex(0);
+     end = NodeIndex(0);
+   }
+   else{
+     NodeIndex(size-1).set(value);
+     NodeIndex(size-1).setNext(null);
+     NodeIndex(size-2).setNext(value);
+     end = NodeIndex(size-1);
+   }
+   return true;
  }
+
+
+
+
  public void add(int index, String value)
  {
-  size++;
-  String current = value;
+   if(index == size-1)
+   {
+     add(value);
+   }
+   
+   size++;
+   String current = value;
+
+// Empty List
+   if(size == 1)
+   {
+     NodeIndex(0).setNext(null);
+     NodeIndex(0).setPrev(null);
+     NodeIndex(0).set(value);
+     start = NodeIndex(0);
+     end = NodeIndex(0);
+   }
+
+
+// Operating on the Head
+
   for(int i = size - 2; i > index; i--)
   {
-    NodeIndex(i).setNext(value);
+  //  [1]->[2] [2]->[103] [103]->[4] [4]->[null] [...]
+  //  [Value]->[1] [1]->[2] [2]->[103] [103]->[4] [4]->[null]
+
+    NodeIndex(i).setNext(NodeIndex(i).get(i));
+    NodeIndex(i).set(NodeIndex(i).getPrev(i));
   }
   NodeIndex(index).set(current);
+
+  if(index == 0)
+  {
+    start = NodeIndex(index);
+    NodeIndex(index).setPrev(null);
+  }
+
  }
 
  public String get(int index)
