fe4b39f96063afeb7482142de85a0662a17e5d49
Jonah Keller <jkeller10@stuy.edu>
2021-01-10 22:02:09 -0500

    Implemented add

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -22,7 +22,13 @@ public class MyLinkedList{
  }
  public void add(int index, String value)
  {
-
+  size++;
+  String current = value;
+  for(int i = size - 2; i > index; i--)
+  {
+    NodeIndex(i).setNext(value);
+  }
+  NodeIndex(index).set(current);
  }
 
  public String get(int index)
@@ -40,12 +46,23 @@ public class MyLinkedList{
    return "not finished";
  }
 
+ public String remove(int index)
+ {
+   return "not finished";
+
+ }
+
 
 
  //Any helper method that returns a Node object MUST BE PRIVATE!
 
 private Node NodeIndex(int index)
 {
+  if(index >= size || index < 0)
+  {
+    throw new IndexOutOfBoundsException("Zero PR");
+  }
+
   Node current = start;
   for(int i = 0; i < index; i++)
   {
